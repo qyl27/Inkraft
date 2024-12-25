cccbefd8c3afdcf2b28a9abe3d78b64b6cdd5357
秋雨落 <i@rain.cx>
2024-12-25 14:05:54 +0800

    [F] oh, ok, really a f**king script language I had ever seen.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/inkraft/utility/StringArgumentParseHelper.java
+++ src/main/java/cx/rain/mc/inkraft/utility/StringArgumentParseHelper.java
@@ -1,5 +1,6 @@
 package cx.rain.mc.inkraft.utility;
 
+import com.google.common.collect.ImmutableMap;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.arguments.NbtPathArgument;
 import net.minecraft.core.Holder;
@@ -12,7 +13,45 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
+import java.util.Map;
+
 public class StringArgumentParseHelper {
+    private static final Map<Character, Character> ESCAPING_CHARS = ImmutableMap.<Character, Character>builder()
+            .put('@', '@')
+            .put('<', '{')
+            .put('>', '}')
+            .put('q', '"')
+            .put('d', '\'')
+            .build();
+
+    public static String unescape(String str) {
+        var builder = new StringBuilder();
+        var it = str.codePoints().iterator();
+        var escaping = false;
+        while (it.hasNext()) {
+            var codePoint = it.next();
+            var ch = Character.toChars(codePoint);
+
+            if (ch.length != 1) {
+                continue;
+            }
+
+            if (codePoint == '@' && !escaping) {
+                escaping = true;
+                continue;
+            }
+
+            if (ESCAPING_CHARS.containsKey(ch[0]) && escaping) {
+                builder.append(ESCAPING_CHARS.get(ch[0]));
+                escaping = false;
+                continue;
+            }
+
+            builder.append(ch);
+        }
+        return builder.toString();
+    }
+
     public static Holder<Item> parseItem(HolderLookup.Provider registries, String id) {
         var rl = ResourceLocation.parse(id);
         return registries.lookupOrThrow(Registries.ITEM).getOrThrow(ResourceKey.create(Registries.ITEM, rl));
@@ -25,7 +64,8 @@ public class StringArgumentParseHelper {
 
     public static NbtPathArgument.NbtPath parseNbtPath(String value) {
         try {
-            return NbtPathArgument.NbtPath.of(value);
+            var unescaped = unescape(value);
+            return NbtPathArgument.NbtPath.of(unescaped);
         } catch (CommandSyntaxException ex) {
             throw new RuntimeException(ex);
         }
@@ -33,7 +73,8 @@ public class StringArgumentParseHelper {
 
     public static Tag parseNbt(String value) {
         try {
-            return TagParser.parseTag(value);
+            var unescaped = unescape(value);
+            return TagParser.parseTag(unescaped);
         } catch (CommandSyntaxException ex) {
             throw new RuntimeException(ex);
         }
---
+++ src/test/resources/sources/test5.ink
@@ -0,0 +1,30 @@
+INCLUDE engine_functions_include.ink
+
+测试：
+* [12金苹果换1金钻石] -> buy_1
+* [1钻石换12金苹果] -> buy_2
+
+=== buy_1 ===
+~ temp has = hasItem("minecraft:apple", 12, "components", "@<@qminecraft:custom_name@q:@d@<@qcolor@q:@qgold@q,@qitalic@q:false,@qtext@q:@q金苹果@q@>@d,@qminecraft:lore@q:[@d@<@qcolor@q:@qaqua@q,@qitalic@q:false,@qtext@q:@q名字是金色的字的苹果@q@>@d]@>")
+
+变量 has： {has}
+
+{ has:
+    ~ temp took = takeItem("minecraft:apple", 12, "components", "@<@qminecraft:custom_name@q:@d@<@qcolor@q:@qgold@q,@qitalic@q:false,@qtext@q:@q金苹果@q@>@d,@qminecraft:lore@q:[@d@<@qcolor@q:@qaqua@q,@qitalic@q:false,@qtext@q:@q名字是金色的字的苹果@q@>@d]@>")
+    变量 took： {took}
+    { took:
+        ~ temp gave = giveItem("minecraft:diamond", 1, "components", "@<@qminecraft:custom_name@q:@d@<@qcolor@q:@qgold@q,@qitalic@q:false,@qtext@q:@q金钻石@q@>@d,@qminecraft:lore@q:[@d@<@qcolor@q:@qaqua@q,@qitalic@q:false,@qtext@q:@qqyl给的钻石@q@>@d]@>")
+        变量 gave： {gave}
+        -> DONE
+    - else:
+        失败！
+        -> DONE
+    }
+- else:
+    失败！
+    -> DONE
+}
+
+=== buy_2 ===
+先不换这个了（（（
+-> DONE
